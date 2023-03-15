db753b8d37b05295d0e9846007925f9be3432f6b
mounyaze <mounya.zekraoui@enim.ac.ma>
2023-03-15 09:49:17 +0100

    Ajoutez des fichiers projet.

---
+++ RestaurantApp.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantApp.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id=1, Name="Le Merisier", Cuisine=CuisineType.French},
+                new Restaurant { Id=2, Name="Abtal Cham", Cuisine=CuisineType.Syrian},
+                new Restaurant { Id=3, Name = "Dar Al Naji", Cuisine =CuisineType.Moroccan}
+            };
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+
+        }
+    }
+
+}
